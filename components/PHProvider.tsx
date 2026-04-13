3ae153ba1cef7aee408a196a7a0106787c6f5d1f
Khadimou <khadimou-rassoul.diop@ens.uvsq.fr>
2026-04-13 15:53:33 +0200

    fix: init PostHog at module load to ensure events are captured

--- components/PHProvider.tsx
+++ components/PHProvider.tsx
@@ -2,32 +2,18 @@
 
 import { useEffect } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
-import { initPostHog, posthog } from "@/lib/posthog";
+import { posthog } from "@/lib/posthog";
 
-function PostHogPageView() {
+export default function PHProvider({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    if (!pathname) return;
     const url = searchParams.toString()
       ? `${pathname}?${searchParams.toString()}`
       : pathname;
     posthog.capture("$pageview", { $current_url: url });
   }, [pathname, searchParams]);
 
-  return null;
-}
-
-export default function PHProvider({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    initPostHog();
-  }, []);
-
-  return (
-    <>
-      <PostHogPageView />
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
